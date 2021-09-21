3e7e19c69e4c9e72cd9198db8436031563700ed3
Paulo Rodrigues <paulo.rodrigues@tre-rr.jus.br>
2021-09-21 11:30:23 -0400

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,63 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	// Obtendo uma conexão de banco de dados
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");			
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				
+				throw new DBException(e.getMessage()); 
+			}
+		}
+		return conn;
+	}
+	
+	// Fechando a conexão com a base de dados
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {				
+				throw new DBException(e.getMessage());
+			}
+		}
+	}
+	
+	// Fechando o Statement
+	public static void closeStatement(Statement st) {
+		try {
+			st.close();
+		} catch (SQLException e) {
+			
+			throw new DBException(e.getMessage());
+		}
+	}
+	
+	
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DBException(e.getMessage());
+		}
+	}
+
+}
