a90d784860d6ba2a06fb8a56abe6ba1f54dd55e7
Paulo Rodrigues <paulo.rodrigues@tre-rr.jus.br>
2021-09-21 11:39:18 -0400

    Department entity class

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+		
+	}
+
+}
