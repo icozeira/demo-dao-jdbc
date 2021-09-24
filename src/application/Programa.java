fe3d9e91eb655afdac73bfc724fbe306d8b20218
Paulo Cezar Rodrigues da Silva <engenheiro.paulo.cezar@gmail.com>
2021-09-24 15:27:12 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,10 @@ public class Programa {
 		
 		Department department = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
-				
+		
+		
+		SellerDao sellerDao = DaoFactory.createSellerdao();
+		
 		System.out.println(seller);
 		
 	}
